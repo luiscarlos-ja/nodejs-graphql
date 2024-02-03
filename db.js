7092af2a1877463306084cadcbcb4fe60a061b1d
Luis Carlos <luiscarlos_ja@outlook.com>
2024-02-02 23:39:56 -0600

    Format documents with Prettier

--- db.js
+++ db.js
@@ -2,5 +2,11 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI).then(() => { console.log("connected to MongoDB"); })
-.catch((error) => { console.log("error connecting to MongoDB:", error.message); });
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
--- models/person.js
+++ models/person.js
@@ -2,26 +2,26 @@ import mongoose from "mongoose";
 import uniqueValidator from "mongoose-unique-validator";
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 5,
-    },
-    phone: {
-        type: String,
-        minlength: 5,
-    },
-    street: {
-        type: String,
-        required: true,
-        minlength: 5,
-    },
-    city: {
-        type: String,
-        required: true,
-        minlength: 5,
-    },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+  },
+  phone: {
+    type: String,
+    minlength: 5,
+  },
+  street: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
 });
 
 personSchema.plugin(uniqueValidator);
